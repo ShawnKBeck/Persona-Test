be693f92f2c46c7f0d2feb531ffd8352c9adf1d4
ShawnKBeck <48632060+ShawnKBeck@users.noreply.github.com>
2023-07-08 23:05:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
-import os
-import openai
 import streamlit as st
-from streamlit.server.server import Server
+import openai
 
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 
@@ -9,13 +7,6 @@ system_prompt = """..."""  # Include the full system prompt here
 
 # The 'system' message to set up the assistant's behavior
 system_message = {"role": "system", "content": system_prompt}
-
-def get_state():
-    session_id = get_report_ctx().session_id
-    session_info = Server.get_current()._get_session_info(session_id)
-    if session_info is None:
-        raise RuntimeError("Could not get Streamlit session.")
-    return session_info.session._main_dg._get_state()
 
 def ask_gpt3(question, messages):
     user_message = {"role": "user", "content": question}
@@ -28,13 +19,12 @@ def ask_gpt3(question, messages):
 
 st.title('Shawn Beck')
 
-state = get_state()
-
-if 'messages' not in state:
-    state['messages'] = [system_message]
+if 'messages' not in st.session_state:
+    st.session_state['messages'] = [system_message]
 
 question = st.text_input("I am Shawn Beck. Ask my anything:")
 if st.button('Ask'):
-    response = ask_gpt3(question, state['messages'])
-    state['messages'].append({"role": "assistant", "content": response})
+    response = ask_gpt3(question, st.session_state['messages'])
+    st.session_state['messages'].append({"role": "assistant", "content": response})
     st.write(response)
+
